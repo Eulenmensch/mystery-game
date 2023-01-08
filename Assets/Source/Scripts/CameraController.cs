fac5e7c64a0bb5b17f176ed9f43482265a3ce2fc
Yoshua Woo <yoshua.woo@gmail.com>
2023-01-08 18:28:36 +0100

    Added Camera Controller Script

---
+++ Assets/Source/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [field: SerializeField] private float panSmoothing;
+    [field: SerializeField] private float zoomSmoothing;
+
+    private void LateUpdate()
+    {
+        throw new NotImplementedException();
+    }
+}
