7e52dd12fed4adf563b126285699fdbdf7fdd6a9
Yoshua Woo <yoshua.woo@gmail.com>
2023-01-10 14:34:18 +0100

    Wired up tap/click and drag input

--- Assets/Source/Scripts/CameraController.cs
+++ Assets/Source/Scripts/CameraController.cs
@@ -1,15 +1,43 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
     [field: SerializeField] private float panSmoothing;
     [field: SerializeField] private float zoomSmoothing;
 
-    private void LateUpdate()
+    private bool isPressed;
+    private Vector2 lastDelta;
+
+    private void Update()
     {
         throw new NotImplementedException();
+    }
+
+    private void Pan(Vector2 _delta)
+    {
+        
+    }
+
+    public void HandleDragInput(InputAction.CallbackContext _context)
+    {
+        if (isPressed)
+        {
+            Pan(_context.ReadValue<Vector2>());
+        }
+    }
+
+    public void HandlePressInput(InputAction.CallbackContext _context)
+    {
+        if (_context.started)
+        {
+            isPressed = true;
+        }
+
+        if (_context.canceled)
+        {
+            isPressed = false;
+        }
     }
 }
