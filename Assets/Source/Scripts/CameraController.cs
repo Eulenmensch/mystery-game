cff360707c47f33d436af363b17437e4b3c15bf4
Yoshua Woo <yoshua.woo@gmail.com>
2023-01-10 18:34:34 +0100

    Basic Camera Panning

--- Assets/Source/Scripts/CameraController.cs
+++ Assets/Source/Scripts/CameraController.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,23 +8,29 @@ public class CameraController : MonoBehaviour
     [field: SerializeField] private float zoomSmoothing;
 
     private bool isPressed;
-    private Vector2 lastDelta;
+    private Vector2 lastPos;
+    private float cameraInertia;
+    private Camera mainCamera;
 
-    private void Update()
+    private void Awake()
     {
-        throw new NotImplementedException();
+        mainCamera = Camera.main;
     }
 
     private void Pan(Vector2 _delta)
     {
-        
+        transform.position += (Vector3)_delta;
     }
 
     public void HandleDragInput(InputAction.CallbackContext _context)
     {
+        var currentPos = _context.ReadValue<Vector2>();
+        Vector2 delta = lastPos - (Vector2)mainCamera.ScreenToWorldPoint(currentPos);
+        // Debug.Log("origin " + lastPos + " to current " + currentPos + " equals a delta of " + delta);
+
         if (isPressed)
         {
-            Pan(_context.ReadValue<Vector2>());
+            Pan(delta);
         }
     }
 
@@ -33,6 +39,8 @@ public class CameraController : MonoBehaviour
         if (_context.started)
         {
             isPressed = true;
+            lastPos = Pointer.current.position.ReadValue();
+            lastPos = mainCamera.ScreenToWorldPoint(lastPos);
         }
 
         if (_context.canceled)
