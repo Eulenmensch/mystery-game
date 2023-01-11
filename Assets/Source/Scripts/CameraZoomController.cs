58e9ce4613419411280aae5ab0a76250f7f15466
Yoshua Woo <yoshua.woo@gmail.com>
2023-01-11 21:23:02 +0100

    Smoothed Zooming for Desktop
    It will have to be tested if the behaviour also works for touch. Google Maps uses different behaviour for scroll wheels and touch pinch gestures.

--- Assets/Source/Scripts/CameraZoomController.cs
+++ Assets/Source/Scripts/CameraZoomController.cs
@@ -1,6 +1,8 @@
+using System;
 using Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using DG.Tweening;
 
 public class CameraZoomController : MonoBehaviour
 {
@@ -9,27 +11,40 @@ public class CameraZoomController : MonoBehaviour
 	[SerializeField] private float zoomSmoothingTime;
 	[SerializeField] private CinemachineVirtualCamera virtualCamera;
 
+	private float cameraZoom;
+
+	private void Start()
+	{
+		cameraZoom = virtualCamera.m_Lens.OrthographicSize;
+	}
+
 	private void Zoom(float _zoomAmount)
 	{
-		var cameraZoom = virtualCamera.m_Lens.OrthographicSize;
-		cameraZoom -= _zoomAmount;
-		if (cameraZoom > maxZoom)
+		cameraZoom = virtualCamera.m_Lens.OrthographicSize;
+		var zoomTarget = cameraZoom - _zoomAmount;
+		// cameraZoom -= _zoomAmount;
+		if (zoomTarget > maxZoom)
 		{
+			DOTween.Kill(cameraZoom);
 			cameraZoom = maxZoom;
 			return;
 		}
 
-		if (cameraZoom < minZoom)
+		if (zoomTarget < minZoom)
 		{
+			DOTween.Kill(cameraZoom);
 			cameraZoom = minZoom;
 			return;
 		}
 
+		DOTween.To(() => cameraZoom, x => cameraZoom = x, zoomTarget, zoomSmoothingTime);
+	}
+
+	private void LateUpdate()
+	{
 		virtualCamera.m_Lens.OrthographicSize = cameraZoom;
 	}
-	
-	
-	
+
 	public void HandleZoomInput(InputAction.CallbackContext _context)
 	{
 		if (_context.performed)
